c3bd290050ada737682e7d5b73b6a17578ffb363
Newton Muchael <newtonmuchael@gmail.com>
2017-01-15 23:31:09 -0200

    Commit inicial

---
+++ src/main/java/br/com/eits/chat_stomp_websocket/Application.java
@@ -0,0 +1,12 @@
+package br.com.eits.chat_stomp_websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/br/com/eits/chat_stomp_websocket/Greeting.java
@@ -0,0 +1,18 @@
+package br.com.eits.chat_stomp_websocket;
+
+public class Greeting {
+
+    private String content;
+
+    public Greeting() {
+    }
+
+    public Greeting(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+}
